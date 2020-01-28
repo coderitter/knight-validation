2a2c08f8c69d96e0f1ec3a7c9c99d2a96b462b90
R Gastmeier <r.gastmeier@gmail.com>
2020-01-29 00:29:22 +0100

    Add Exists to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Constraint } from './lib/Constraint'
+export { default as Exists } from './lib/constraints/Exists'
 export { default as Required } from './lib/constraints/Required'
 export { default as Unique } from './lib/constraints/Unique'
 export { default as Misfit } from './lib/Misfit'
