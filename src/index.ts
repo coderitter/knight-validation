e259eb1782c4ef136086666f3e6d1235bbbcf43e
R Gastmeier <r.gastmeier@gmail.com>
2020-01-28 20:55:47 +0100

    Add Unique and Constraint to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { default as Constraint } from './lib/Constraint'
 export { default as Required } from './lib/constraints/Required'
+export { default as Unique } from './lib/constraints/Unique'
 export { default as Misfit } from './lib/Misfit'
 export { default as Validator, ValidatorOptions } from './lib/Validator'
