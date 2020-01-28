f345efdd246798036c21dbf3622b4c0276b613da
R Gastmeier <r.gastmeier@gmail.com>
2020-01-28 21:13:19 +0100

    Change test function parameter on Unique to be async

--- src/lib/constraints/Unique.ts
+++ src/lib/constraints/Unique.ts
@@ -3,15 +3,15 @@ import Misfit from '../Misfit'
 
 export default class Unique extends Constraint {
 
-  isUnique: (value: any) => boolean
+  isUnique: (value: any) => Promise<boolean>
 
-  constructor(isUnique: (value: any) => boolean) {
+  constructor(isUnique: (value: any) => Promise<boolean>) {
     super()
     this.isUnique = isUnique
   }
 
   async validate(value: any): Promise<Misfit|undefined> {
-    if (! this.isUnique(value)) {
+    if (! await this.isUnique(value)) {
       return new Misfit(this.name)
     }
   }
