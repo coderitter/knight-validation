6c5f803a06454e55c30c3dadd691c1aaa98556a5
R Gastmeier <r.gastmeier@gmail.com>
2020-01-29 02:53:00 +0100

    Add TypeOf to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { default as Constraint } from './lib/Constraint'
 export { default as Exists } from './lib/constraints/Exists'
 export { default as Required } from './lib/constraints/Required'
+export { default as TypeOf } from './lib/constraints/TypeOf'
 export { default as Unique } from './lib/constraints/Unique'
 export { default as Misfit } from './lib/Misfit'
 export { default as Validator, ValidatorOptions } from './lib/Validator'
+
